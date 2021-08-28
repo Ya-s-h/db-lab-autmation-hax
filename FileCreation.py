3a0b560e9adc23a5ac99a794b8f0b3c75714c978
Yash <vasu2013agg@gmail.com>
2021-08-28 16:51:22 +0530

    MInor Error Fixes

--- FileCreation.py
+++ FileCreation.py
@@ -17,6 +17,7 @@ def dataInsertion(Data: list):
             # print(dic.values())
             fp.write("Insert into labreport(")
             for countKeys, keys in enumerate(dic.keys()):
+
                 if countKeys != len(dic) - 1:
                     fp.write("%s ," % keys)
                 else:
@@ -25,13 +26,69 @@ def dataInsertion(Data: list):
             fp.write(str(count + 1) + ", ")
             for countValues, value in enumerate(dic.values()):
                 if countValues != len(dic) - 1:
-
-                    fp.write("%s ," % value)
+                    if type(value) == str:
+                        fp.write("'%s' ," % value)
+                    else:
+                        fp.write("%s ," % value)
                 else:
-                    fp.write("%s" % value)
+                    if type(value) == str:
+                        fp.write("'%s'" % value)
+                    else:
+                        fp.write("%s" % value)
             fp.write(");\n")
 
 
-test_data = [{"A": "First", "a": "first"}, {"B": "Second"}, {"C": "third"}]
+test_data = [{'assigned_date': 'NULL',
+  'problem_desc': '1.1 Create table',
+  'status': True,
+  'submission_date': '2021-08-30T23:55:00'},
+ {'assigned_date': 'NULL',
+  'problem_desc': '1.2 Insert records',
+  'status': True,
+  'submission_date': '2021-08-30T23:55:00'},
+ {'assigned_date': 'NULL',
+  'problem_desc': '1.3 Update Table',
+  'status': True,
+  'submission_date': '2021-08-30T23:55:00'},
+ {'assigned_date': 'NULL',
+  'problem_desc': '1.4 Select all records from table',
+  'status': True,
+  'submission_date': '2021-08-30T23:55:00'},
+ {'assigned_date': 'NULL',
+  'problem_desc': '1.5 Delete rows from table',
+  'status': True,
+  'submission_date': '2021-08-30T23:55:00'},
+ {'assigned_date': 'NULL',
+  'problem_desc': '2.1 Select particular records from a table.',
+  'status': True,
+  'submission_date': '2021-08-29T23:55:00'},
+ {'assigned_date': 'NULL',
+  'problem_desc': '2.2 Select distinct records from a table',
+  'status': True,
+  'submission_date': '2021-09-03T23:55:00'},
+ {'assigned_date': 'NULL',
+  'problem_desc': '2.3 Delete particular records from a table',
+  'status': True,
+  'submission_date': '2021-09-03T23:55:00'},
+ {'assigned_date': 'NULL',
+  'problem_desc': '2.4 Update particular records from a table',
+  'status': True,
+  'submission_date': '2021-09-03T23:55:00'},
+ {'assigned_date': 'NULL',
+  'problem_desc': '2.5 Filter records part I',
+  'status': True,
+  'submission_date': '2021-09-03T23:55:00'},
+ {'assigned_date': 'NULL',
+  'problem_desc': '2.6 Filter records part II',
+  'status': True,
+  'submission_date': '2021-09-03T23:55:00'},
+ {'assigned_date': 'NULL',
+  'problem_desc': '2.7 Filter records - part III',
+  'status': True,
+  'submission_date': '2021-09-03T23:55:00'},
+ {'assigned_date': 'NULL',
+  'problem_desc': '2.8 Sort table in ascending order',
+  'status': True,
+  'submission_date': '2021-08-29T23:55:00'}]
 
 dataInsertion(test_data)
